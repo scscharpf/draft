bd065f03dabfd80360c4fadce359f3095d11e59d
Cabo, Selma <selma.cabo@metoffice.gov.uk>
2017-11-02 11:55:29 +0000

    removed packages-dir

--- shredMetadata_setup.py
+++ shredMetadata_setup.py
@@ -5,10 +5,8 @@ setup(name='shred_metadata',
       packages=['src',
                 'src.shredMetadata',
                 'src.restHandlers',
-                'src.awsBase',
-                'package-dir'],
+                'src.awsBase'],
       install_requires=['netCDF4',
                         'xarray'],
-      packages_dir={'src': 'src',
-                    'package-dir': 'package-dir'}
+      packages_dir={'src': 'src'}
       )
